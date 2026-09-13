73f9cf91908a1f6bc66d23e6b0af9be3ef640a5a
Manos Kounelakis <pkounelios@gmail.com>
2026-09-13 21:53:20 +0300

    Simplify delegates and code refactor .

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,63 +1,71 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ClassReflectionKit.Extensions;
 using ClassReflectionKit.Helpers;
-using ClassReflectionKit.Models;
 using ClassreflectionUtilLib.Models;
 
-
-
-var classInfoDelegate = new ProcessClassInfo(e => { 
-    if (e == null)
-    {
-        return null;
-    }
-    e.ClassProperties.ToList()
-        .ForEach(x => x.IsNullable = false);
-    var nullableProps = e.ClassProperties.Where(x => x.PropName.EndsWith("Specified"));
-    if (nullableProps.Count() < 1) return e;
-    foreach (var prop in nullableProps)
-    {
-        // Find corresponding property that  does not end with "Specified"
-        var propName = prop.PropName.Replace("Specified", "");
-        var correspondingProp = e.ClassProperties.FirstOrDefault(x => x.PropName == propName);
-        if (correspondingProp == null) continue;
-        correspondingProp.IsNullable = true;
-    }
-    // Keep only properties that do not end with "Specified"
-    e.ClassProperties = e.ClassProperties.
-        Where(x => !nullableProps.Select(x => x.PropName).Contains(x.PropName))
-        .ToList();
-    return e; 
-});
-
-var processNSClassesDelegate = new ProcessNSClasses(e => { return e; });
-
+// #region Helper Init
 var helper = new ClassReflectionKitHelper();
+var customHelper = new CustomHelper();
+// Resolve files relative to the deployed application, where Samples is copied by the project file.
+var samplesDirectory = Path.Combine(AppContext.BaseDirectory, "Samples");
+var samples = new List<string> { "Address.cs", "User.cs" , "Employee.cs" }
+    .Select(x => Path.Combine(samplesDirectory, x)).ToList();
 
+var codeSamples = samples.Select(File.ReadAllText).ToArray();
+helper.InitializeFromCodeInputs(codeSamples);
+customHelper.InitializeFromCodeInputs(codeSamples);
+// #endregion
 
-var samples = new List<string> { "Address.cs", "User.cs" , "Employee.cs" };
-//Map each sample file to combine it with the current directory
-samples = samples.Select(x => Path.Combine(Directory.GetCurrentDirectory(), "Samples", x)).ToList();
-helper.InitializeFromFilePaths(samples.ToArray());
-foreach (var ns in helper.GetNameSpaces())
+// #region Delegates
+
+static ClassReflectionKit.Models.ProcessClassInfo HandleNullableProps()
 {
-    Console.WriteLine("Namespace: " + ns);
-
-    var classes = helper.GetNSClasses(ns);
-    foreach (var cl in classes)
+    return (c) =>
     {
-        Console.WriteLine(cl.ToTemplateString());
-    }
+        var props = c.ClassProperties.Select(s => s.PropName).ToArray();
+        var formatted = string.Join(',', props);
+        var nullableProps = props.Where(s => s.EndsWith("Specified")).ToArray();
+        foreach (var nprop in nullableProps)
+        {
+            var correspondingPropName = nprop.Replace("Specified", "");
+            Console.WriteLine($"Will edit prop {correspondingPropName}");
+            var prop = c.ClassProperties.Find(e => e.PropName == correspondingPropName);
+            if (prop == null) continue;
+            prop.IsNullable = true;
+        }
+        c.ClassProperties = c.ClassProperties.Where(
+            prop => !nullableProps.Contains(prop.PropName)
+        ).ToList();
+        return c;
+    };
 }
 
 
-var fromCodeHelper = new CustomHelper();
-var codeSamples = samples.Select(x => File.ReadAllText(x)).ToArray();
-fromCodeHelper.InitializeFromCodeInputs(codeSamples);
-foreach (var ns in fromCodeHelper.GetNameSpaces())
+static ClassReflectionKit.Models.ProcessNSClasses HandleCustomPropsClasses()
+{
+    return e =>
+    {
+        var availableClassNames = e.Select(s => s.ClassName).ToArray();
+        foreach(var c in e)
+        {
+            // Iterate through each prop. If the propTypeName is included in the availableClassNames mark it as CustomType
+            foreach(var prop in c.ClassProperties){
+                if(availableClassNames.Contains(prop.PropTypeName)){
+                    prop.IsCustomClass = true;
+                }
+            }
+        }
+        return e;
+    };
+}
+// #endregion
+
+
+foreach (var ns in helper.GetNameSpaces())
 {
     Console.WriteLine("Namespace: " + ns);
-    var classes = fromCodeHelper.GetNSClasses(ns);
+    var classes = helper.GetNSClasses(ns, HandleCustomPropsClasses(), HandleNullableProps())
+        .ToArray();
     foreach (var cl in classes)
     {
         Console.WriteLine(cl.ToTemplateString());
